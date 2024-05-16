88a4188f3e453779fc76c46b0c88efcc5fb68cf6
Ryanduran001 <112416371+Ryanduran001@users.noreply.github.com>
2024-05-16 15:12:55 -0700

    More JS for countdown and format
    Finn Poduska's Commit

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,20 @@
-
-
-// while (true) {
-//     let time = new Date()
-//     console.log(time.getMinutes())
-// }
-
-
-
-
-
-
-
-
-
+const schedules = {
+    standard: {
+        FirstPassing: [8, 30, 0],
+        First: [9, 25, 0],
+        SecondPassing: [9, 32, 0],
+        Second: [10, 31, 0],
+        ThirdPassing: [10, 38, 0],
+        Third: [11, 33, 0],
+        FourthPassing: [11, 40, 0],
+        Fourth: [12, 35, 0],
+        Lunch: [13, 13, 0],
+        SixthPassing: [13, 20, 0],
+        Sixth: [14, 14, 0],
+        SeventhPassing: [14, 21, 0],
+        Seventh: [15, 15, 0],
+    }
+}
 
 function count() {
     const theDate = new Date().toLocaleDateString('en-us', { weekday:"short", month:"numeric", day:"numeric"})
@@ -20,6 +22,27 @@ function count() {
     let hrs = (time.getHours());
     let min = (time.getMinutes());
     let sec = (time.getSeconds());
+
+    // let target = [12, 37, 0]
+    // let target = [10, 45, 0]
+    let target = schedules.standard.Seventh
+
+    hrsD = (target[0] - parseInt(hrs));
+    minD = (target[1] - parseInt(min));
+    if(minD < 0) {
+        hrsD--
+        minD += 60;
+    }
+    secD = (target[2] - parseInt(sec));
+    if(secD < 0) {
+        minD--
+        secD += 60;
+    }
+    
+    // Format
+    if (hrs > 12) {
+        hrs = hrs - 12
+    };
     if (hrs < 10) {
         hrs = '0'+hrs
     };
@@ -29,18 +52,42 @@ function count() {
     if (sec < 10) {
         sec = '0'+sec
     };
-
-    // let target = [12, 36, 60]
-    let target = [10, 45, 60]
-    hrs = (target[0] - parseInt(hrs));
-    min = (target[1] - parseInt(min));
-    if(min < 0) {
-        hrs--
-        min += 60;
+    if (hrsD < 10) {
+        hrsD = '0'+hrsD
+    };
+    if (hrsD > 12) {
+        hrsD = hrsD - 12
+    };
+    if (minD < 10) {
+        minD = '0'+minD
+    };
+    if (secD < 10) {
+        secD = '0'+secD
+    };
+    
+    let targetHrs = target[0];
+    if (targetHrs > 12) {
+        targetHrs = targetHrs - 12
+    };
+    if (targetHrs < 10) {
+        targetHrs = '0'+targetHrs
     }
-    sec = (target[2] - parseInt(sec));
+    let targetMin = target[1];
+    if (targetMin < 10) {
+        targetMin = '0'+targetMin
+    }
+    let targetSec = target[2];
+    if (targetSec < 10) {
+        targetSec = '0'+targetSec
+    }
+
+    // countDown
+    document.getElementById(`countDown`).innerText = `${hrsD}:${minD}:${secD} until ${targetHrs}:${targetMin}:${targetSec}`;
+    // console.log(`${hrsD}:${minD}:${secD}`)
+
+    // date
+    document.getElementById(`dateTime`).innerText = `${theDate}, ${hrs}:${min}:${sec}`;
     // console.log(`${theDate}, ${hrs}:${min}:${sec}`)
-    console.log(`${hrs}:${min}:${sec}`)
 }
 
 setInterval(count, 1000)
